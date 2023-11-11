88007dd3153da986ed8ff93461f4e881cdbcd554
Achim Lenz <hello.achim@outlook.com>
2023-11-11 16:49:38 +0100

    add second test for method addMenu()

--- src/test/java/bootcamp/neuefische/OrderSystemTest.java
+++ src/test/java/bootcamp/neuefische/OrderSystemTest.java
@@ -10,13 +10,26 @@ import static org.junit.jupiter.api.Assertions.*;
 class OrderSystemTest {
 
     @Test
-    void addMenu_whenAdd1Element_thenSizeIs1(){
+    void addMenu_whenAdding1Element_thenSizeIs1(){
         // GIVEN
-        Menu menu = new Menu("Beef Burger Menu", 17, "Double Trouble", "Homestyle Fries", "Fanta");
         int expected = 1;
         // WHEN
+        Menu menu = new Menu("Beef Burger Menu", 17, "Double Trouble", "Homestyle Fries", "Fanta");
         OrderSystem.addMenu("101", menu);
         int actual = OrderSystem.menus.size();
+        // THEN
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void addMenu_whenAddingMenu101_thenEqualsMenu101(){
+        // GIVEN
+        Map<String, Menu> expected = new HashMap<>();
+        Menu menu = new Menu("Beef Burger Menu", 17, "Double Trouble", "Homestyle Fries", "Fanta");
+        expected.put("101", menu);
+        // WHEN
+        OrderSystem.addMenu("101", menu);
+        Map<String, Menu> actual = OrderSystem.menus;
         // THEN
         assertEquals(expected, actual);
     }
